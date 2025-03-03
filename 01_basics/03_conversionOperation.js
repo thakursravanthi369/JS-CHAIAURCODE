f4d8265bd55b96240c3a50151b7f465c115bd315
THAKUR SRAVANTHI <thakursravanthi1111@outlook.com>
2025-03-03 13:14:28 +0000

    basics

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+let score = "36"
+
+// console.log(typeof score); //method-1 for knowing which type of data it is...
+// console.log(typeof(score)); //method-2  for knowing which type of data it is in parenthesis...
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+<--for Number-->
+
+let score = "36" //O/P 36
+let score = "36ab" //o/p NaN(not a nummber) but the type is number only....
+let score = null //o/p 0
+let score = true // o/p 1 //false o/p 0
+let score = undefined //NaN
+let score = "THAKUR" //NaN
+
+
+<--for Boolean-->
+
+let isLoggedIn = 1
+let isLoggedIn = "" // o/p false
+let isLoggedIn = "thakur" //o/p true
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+let someNumber = 27
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
