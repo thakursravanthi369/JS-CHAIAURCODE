b05f17f61aaabdef3a9ea844029501460fa31125
THAKUR SRAVANTHI <thakursravanthi1111@gmail.com>
2025-03-06 21:10:16 +0000

    06_nums_and_math.js

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,35 @@
+// const score = 400 // its gave direct results....
+// console.log(score);
+
+
+// const balance = new Number(100) // when u define in new object its specify the value....
+// console.log(balance);
+// console.log(balance.toString().length); //toString use to convernt the string...
+// console.log(balance.toFixed(2)); //!mostly used method toFixed...
+
+// // const otherNumber = 12.369
+// // console.log(otherNumber.toPrecision()); //!toPrecision(precision?: number): string
+// //!Number of significant digits. Must be in the range 1 - 21, inclusive.Returns a string containing a number represented either in exponential or fixed-point notation with a specified number of digits.
+
+// // const otherNumber = 122.369
+// // console.log(otherNumber.toPrecision(3));
+
+// // const otherNumber = 1225.969
+// // console.log(otherNumber.toPrecision(4));
+
+// const otherNumber = 1225.969
+// console.log(otherNumber.toPrecision(3)); 
+
+// const hunders = 1000000
+
+// //!toLocalString gives commas to user long digit numbers...
+
+// console.log(hunders.toLocaleString()); //for US standards...
+// console.log(hunders.toLocaleString('en-IN')); //for INDIAN standars...
+
+
+// ============================== Maths =================================
+
+
+
+
