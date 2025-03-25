7b054e4fc1a56364de76e0f15c3312775b6b7685
THAKUR SRAVANTHI <thakursravanthi1111@gmail.com>
2025-03-26 00:53:25 +0530

    03_truthy.js

--- 04_controlflow/03_truthy.js
+++ 04_controlflow/03_truthy.js
@@ -19,3 +19,77 @@ if (userEmail) {
 //     console.log("Array is empty");
 // }
 
+// if (userEmail.length) {
+//     console.log("Array is not empty");   
+// }
+
+const objempty = {}
+if (Object.keys(objempty).length === 0) {
+    console.log("Object is empty");
+} else {
+    console.log("Object is not empty");
+}
+
+//interveiw question
+
+//false == 0 //o/p is true
+//false === 0 //o/p is false
+//false == "" //o/p is true
+//false === "" //o/p is false
+//false == [] //o/p is true
+//false === [] //o/p is false
+//false == {} //o/p is false
+//false === {} //o/p is false
+//false == null //o/p is false
+//false === null //o/p is false
+//false == undefined //o/p is false
+//false === undefined //o/p is false
+//false == NaN //o/p is false
+//false === NaN //o/p is false
+//false == false //o/p is true
+//false === false //o/p is true
+//false == true //o/p is false
+//false === true //o/p is false
+//false == "false" //o/p is false
+//false === "false" //o/p is false
+//false == "0" //o/p is true
+//false === "0" //o/p is false    
+//false == " " //o/p is true
+//false === " " //o/p is false
+//false == "null" //o/p is false
+//false === "null" //o/p is false
+//false == "undefined" //o/p is false
+//false === "undefined" //o/p is false
+//false == "NaN" //o/p is false
+//false === "NaN" //o/p is false
+//false == "true" //o/p is false
+//false === "true" //o/p is false
+
+
+// Nullish coalescing operator (??) : null and undefined
+
+let val1 ;
+val1 = 50 ?? 100
+console.log(val1);
+
+let val2 ;
+val2 = null ?? 100  
+console.log(val2);
+
+let val3 ;  
+val3 = undefined ?? 100
+console.log(val3);
+
+let val4 ;
+val4 = null ?? 100 ?? 200 //in this case it takes after null r undefined next value...... 
+console.log(val4);
+
+//if the values r null r undefined then it will take the value after ??
+
+
+//ternary operator 
+
+//condition ? True : False
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("I will buy ice tea less then 100") : console.log("I will not buy ice tea more then 100"); //if the condition is true then it will print the first statement else it will print the second statement.... this is called ternary operator.... and it is used for only one line code.... which is implicit type conversion....
