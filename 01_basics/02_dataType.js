824efd9a3ca98f8411d61ff49245c1f307cf3632
THAKUR SRAVANTHI <thakursravanthi1111@gmail.com>
2025-03-25 08:45:36 +0000

    02_dataType.js

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code a newer version.
+
+// alert (3+6) we are using Node.js , not browser
+
+console.log(3
+    +
+    6); // code readability that should be high
+
+console.log("thakur");
+
+let name = "thakur"
+let age = 24
+let isLoggedIn = flase
+let city;
+
+//number => 2 to power 16
+//bigint => use for big numbering like trading,stockmarket ects.
+//string => ""(prefer) or ''
+//boolean => true/flase
+//null => standalone value (empty value or o/p)
+//undefined =>not definding value like ex:let city;
+//symbol => unique or finding uniqueness 
+
+//object
+
+console.log(typeof "thakur");
+
+console.log(typeof null); //o/p is object
+console.log(typeof undefined); // o/p is undefined bez it is a datatype
