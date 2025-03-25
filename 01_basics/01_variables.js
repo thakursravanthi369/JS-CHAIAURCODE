70ecf7c10449626a9cc99bb48e042ab789a8720c
THAKUR SRAVANTHI <thakursravanthi1111@gmail.com>
2025-03-25 08:47:16 +0000

    01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -17,4 +17,3 @@ accountPassword = "hdfc@12345"
 accountCity = "bengal"
 
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState]); //method 2 with console.table for multiple outputs.
-
