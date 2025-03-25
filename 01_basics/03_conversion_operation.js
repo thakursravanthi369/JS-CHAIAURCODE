cb6a4c5f09ad14c3bc1e8e511813bcc6ddce8762
THAKUR SRAVANTHI <thakursravanthi1111@gmail.com>
2025-03-25 14:32:00 +0000

    03_conversion_operation.js

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,92 @@
+// let score = "36"
+
+// console.log(typeof score); //method-1 for knowing which type of data it is...
+// console.log(typeof(score)); //method-2  for knowing which type of data it is in parenthesis...
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// <--for Number-->
+
+// let score = "36" //O/P 36
+// let score = "36ab" //o/p NaN(not a nummber) but the type is number only....
+// let score = null //o/p 0
+// let score = true // o/p 1 //false o/p 0
+// let score = undefined //NaN
+//  let score = "THAKUR" //NaN 
+
+
+// <--for Boolean-->
+
+// let isLoggedIn = 1
+// let isLoggedIn = "" // o/p false
+// let isLoggedIn = "thakur" //o/p true
+
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// let someNumber = 27
+
+// let stringsomeNumber = String(someNumber)
+// console.log(stringsomeNumber);
+// console.log(typeof stringsomeNumber);
+
+// ****************************  Operations  *******************************************
+
+// let value = 3
+// let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%2);
+
+// let str1 = "hi "
+// let str2 = "reet"
+
+// let str3 = (str1 + str2)
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log((1+1)-2*3%5);
+
+// console.log(+true);
+// console.log(""+);
+
+// let num1,num2,num3
+
+// num1 = num2 = num3 = 2+2
+
+let gameCounter = 90
+
+++gameCounter;
+gameCounter++;
+console.log(gameCounter); //study about the prefix and postfix from MDN
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
