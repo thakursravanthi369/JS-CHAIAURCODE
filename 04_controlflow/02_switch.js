fba68d95eae57c1c57197860b5bdffddfb09777b
THAKUR SRAVANTHI <thakursravanthi1111@gmail.com>
2025-03-25 15:14:32 +0000

    02_switch.js

---
+++ 04_controlflow/02_switch.js
@@ -0,0 +1,50 @@
+//switch statement
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 5  
+
+switch (month){
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+
+    default:
+        break;
+}
+
+
+const week = "monday"  //if the value is string in case sensitivity the value shpuld also be string......so while writing the code have to check.....
+
+switch (week){
+    case "monday":
+        console.log("monday");
+        break;
+    case "tuesday":
+        console.log("tuesday");
+        break;
+    case "wednesday":
+        console.log("wednesday");
+        break;
+
+        default:
+            break;
+    }
