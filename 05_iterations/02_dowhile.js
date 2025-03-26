a1b9b3b92a6293e5adf13a8152081352dc246967
THAKUR SRAVANTHI <thakursravanthi1111@gmail.com>
2025-03-27 00:04:36 +0530

    02_dowhile.js

---
+++ 05_iterations/02_dowhile.js
@@ -0,0 +1,47 @@
+//while loop syntax
+
+// while (condition) {
+    
+// }
+
+// let index = 0
+
+// while (index < 10) {
+//     console.log(`value is ${index}`)
+//     index = index + 2    //if we don't increment r decrement the value of index, it will run infinitely
+// }
+
+
+// let myArray = ['apple', 'banana', 'mango', 'orange', 'grapes']
+
+// let arr = 0
+// while (arr<= myArray.length) {
+//     console.log(`value is ${myArray[arr]}`)
+//     arr = arr + 1
+    
+// }
+
+// let me = ["moon", "sun", "star", "earth", "mars"]
+
+// let i = 0 
+// while (i < me.length) {
+//     console.log(`value is ${me[i]}`)
+//     i = i + 4
+// }
+
+
+//do while loop syntax
+
+// do {     //do while loop will execute atleast once
+    
+// } while (condition);  //condition is checked after the loop is executed
+
+
+let life = 10
+
+do{
+    console.log(`value is ${life}`) 
+    life = life++
+}while (life <= 10) {
+    
+}
