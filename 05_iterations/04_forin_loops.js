2b0d526c2fe909b59c7a9c177a0c7da96460b828
THAKUR SRAVANTHI <thakursravanthi1111@gmail.com>
2025-03-27 14:38:13 +0530

    04_forin_loops.js

---
+++ 05_iterations/04_forin_loops.js
@@ -0,0 +1,57 @@
+//object of strins forin loop
+
+const languages = {
+    south: "telugu",
+    north: "hindi",
+    east: "bengali",
+    west: "marathi"
+};
+
+// for (const key in languages) {  // key is a variable name, it can be anything
+//     console.log(key);   
+//     }
+
+    // for (const key in languages) {
+    //     console.log(key, languages[key]); //myObject[key] is used to access the value of the key in the object
+         
+    //     }
+
+    // for (const key in languages) {
+    //     console.log(`${key} is spoken in ${languages[key]}`); 
+            
+    //     }
+    
+
+    //array of strings in forin loop
+
+    const programming = ["java", "python", "javascript", "c++", "c#"];
+
+    // for (const key in programming ) {
+    //     console.log(key);  // key is the index of the array like 0, 1, 2, 3, 4......
+            
+    //     }
+
+    //  for (const key in programming ) {
+    //      console.log(key, programming[key]);  //m1
+    //  }
+    
+    //  for (const key in programming) {
+    //     console.log(`${programming[key]} is a programming language`);  //m2
+        
+    //     }
+     
+//         const map = new Map();
+ 
+//         map.set("name", "John");
+//            map.set("age", 30);
+//            map.set(true, "yes");
+//            map.set(false, "no");
+          
+// for (const key in map) {
+//   console.log(map);   //maps are not iterable so it will not work 
+  
+//     }   
+
+
+
+    
