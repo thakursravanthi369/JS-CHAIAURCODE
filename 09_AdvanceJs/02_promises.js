504bc1c348d624bde84851fb976bbe6a86009c1c
THAKUR SRAVANTHI <thakursravanthi1111@gmail.com>
2025-04-16 16:10:08 +0000

    02_promises.js

--- 09_AdvanceJs/02_promises.js
+++ 09_AdvanceJs/02_promises.js
@@ -104,17 +104,28 @@ async  function cosumePromiseFive(){     //v can also get async method....
 }
 
 
-cosumePromiseFive()
+// cosumePromiseFive()
 
-async function getAllUsers(){
-try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users')
-        const data =await response.json()
-        console.log(data); 
-} catch (error) {
-    console.log('E: ',error);
+// async function getAllUsers(){
+// try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data =await response.json()
+//         console.log(data); 
+// } catch (error) {
+//     console.log('E: ',error);
     
-}
-}
+// }
+// }
 
-getAllUsers()
+// getAllUsers()
+
+//One more method....with fetch
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);  
+})
+.catch((error) => console.log(error));
